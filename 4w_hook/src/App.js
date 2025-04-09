2286b86066865a18a8f62d7e37b4e4e58155399d
sia <piao516@naver.com>
2025-04-10 04:47:12 +0900

    commit

---
+++ 4w_hook/src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+import TimerComponent from "./TimerComponent";
+
+function App() {
+  return <TimerComponent></TimerComponent>;
+}
+
+export default App;
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import CommentList from "./CommentList";
-
-function App() {
-  return <CommentList>hi</CommentList>;
-}
-
-export default App;
